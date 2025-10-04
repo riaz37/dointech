00776600cf05ce615174e7627f291a37214b86a0
riaz37 <riaz37.ipe@gmail.com>
2025-10-04 23:04:23 +0600

    fix: updated cors issue

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   // Enable CORS for frontend communication
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://dointech.vercel.app'],
     credentials: true,
   });
 
